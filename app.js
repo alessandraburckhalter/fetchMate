5ad7e615d8b102e20e79058219cc80de08425f2f
Pete Looney <petelooney81@gmail.com>
2020-11-23 14:27:04 -0600

    initialized skills route

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
 // var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
 //! Replace with our route files
+const skillsRouter = require('./routes/skills');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -18,6 +19,10 @@ app.use(express.static(path.join(__dirname, 'client/build')));
 //! Replace with our routes
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
+
+//* Skills route
+app.use('/api/skills', skillsRouter);
+
 app.use('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
 })
