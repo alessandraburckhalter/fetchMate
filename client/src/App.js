3f9fa63d3548a05447ab71c1548338cb905b6303
Derek James <derekjames1093@gmail.com>
2020-11-27 11:33:55 -0600

    loaded projects onto project template

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react'
 import { Provider, useDispatch, useSelector } from 'react-redux';
 import { Redirect, Route, Switch, useHistory } from 'react-router-dom';
 import './App.css';
@@ -21,42 +21,47 @@ function App() {
 
   useEffect(() => {
     fetch('/api/v1/user/current/')
-    .then(res => res.json())
-    .then(data => {
-      // console.log(data)
-      if(data.error){
-        console.log('no user')
-        dispatch(checked())
-      }else{
-        dispatch(login(data.user))
-      }
-    })
-  },[dispatch])
+      .then(res => res.json())
+      .then(data => {
+        // console.log(data)
+        if (data.error) {
+          console.log('no user')
+          dispatch(checked())
+        } else {
+          // For any consistent state put into this fetch
+          fetch('/api/v1/hub/current')
+            .then(res => res.json())
+            .then(data => {
+              dispatch(login(data))
+            })
+        }
+      })
+  }, [dispatch])
 
-  if(!user.checked){
+  if (!user.checked) {
     return 'Loading!'
   }
   return (
-    
-        <Switch>
-          <Route exact path="/" component={MainPage}/>
-          <Route path="/register" component={SignUpPage}/>
-          
-          
-          {user.loginInfo !== null && (
-            <>
-            <Route path="/hub" component={ProfileSetup}/>
-            <Route path="/projectForm" component={ProjectForm}/>
-            <Route path="/dashboard" component={Dashboard}/>
-            <Route path="/projects" component={Projects}/>
-            <Route path="/interested" component={Interested}/>
-            <Route path="/test" component={Test}/>
-            </>
-          )}
-          <Route>
-            <Redirect to ='/' />
-          </Route>
-        </Switch>
+
+    <Switch>
+      <Route exact path="/" component={MainPage} />
+      <Route path="/register" component={SignUpPage} />
+
+
+      {user.loginInfo !== null && (
+        <>
+          <Route path="/hub" component={ProfileSetup} />
+          <Route path="/projectForm" component={ProjectForm} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/interested" component={Interested} />
+          <Route path="/test" component={Test} />
+        </>
+      )}
+      <Route>
+        <Redirect to='/' />
+      </Route>
+    </Switch>
 
   );
 }
